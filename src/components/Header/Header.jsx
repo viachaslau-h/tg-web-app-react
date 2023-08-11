9552b666e0e1778391260a69b0572b86930e99b2
Vyacheslav-H <slavik.art15@gmail.com>
2023-08-11 17:50:26 +0300

    init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTelegram } from '../../hooks/useTelegram';
+import Button from '../Button/Button';
 
 const Header = () => {
     const {user, onClose} = useTelegram();
